dc8b6af2e8ff31462eaa24bb30dc507c6987e88a
ranggaradiitya <adityarangga990@gmail.com>
2022-05-08 11:43:04 +0700

    update index.js

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ addDataToLocalStorage();
 function addDataToLocalStorage() {
   const isDataAvailable = localStorage.getItem("rajaerba");
   if (isDataAvailable) return;
-  $.getJSON("/rajaerba.json",
+  $.getJSON("/health-laboratory/rajaerba.json",
     function (data) {
       localStorage.setItem("rajaerba", JSON.stringify(data))
     }
