850c9de2b8e28bf25deeade46d0cff6134a0e86b
ranggaradiitya <adityarangga990@gmail.com>
2022-05-08 11:25:26 +0700

    update index.js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 addDataToLocalStorage();
 
 function addDataToLocalStorage() {
-  const isDataAvailable = localStorage.getItem("rajaerba");
-  if (isDataAvailable) return;
-  $.getJSON("../rajaerba.json",
-    function (data) {
-      localStorage.setItem("rajaerba", JSON.stringify(data))
-    }
-  );
+  $(document).ready(function () {
+    const isDataAvailable = localStorage.getItem("rajaerba");
+    if (isDataAvailable) return;
+    $.getJSON("../rajaerba.json",
+      function (data) {
+        localStorage.setItem("rajaerba", JSON.stringify(data))
+      }
+    );
+  });
 }
 
